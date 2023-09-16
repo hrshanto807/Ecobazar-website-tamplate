a04f54e7c389a8085991a4f918e2737d5165ed49
hrsanto <shantosss4907@gmail.com>
2023-09-16 18:56:21 +0600

    solved

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
 (function ($) {
+    $('#stopwatch').countdown('2023/12/31', function (event) {
+        $(this).html(event.strftime('<div> %d<span>:</span> <br> <p>Days</p> </div>' +'<div> %H<span>:</span> <br> <p>Hours</p> </div>' + '<div> %M<span>:</span><br> <p>Min</p></div>' + '<div>%S <br> <p>Sec</p></div>'));
+    });
     $('.popup-new').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -17,18 +20,19 @@
     });
     $('.eco-icon-menu').click(function () {
         $('.eco-hero-catagory-area').slideToggle();
-
+        $('.eco-hero-content-area').toggleClass('eco-hero-content-bg');
+        $('.eco-hero-slider-area').toggleClass('owl-section');
     });
 
     $('.eco-hero-slider-area').owlCarousel({
         loop: true,
-        margin: 0,
+        margin: 10,
         nav: false,
         items: 1,
         dots: true,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
+        // autoplay: true,
+        // autoplayTimeout: 2000,
+        // autoplayHoverPause: true,
 
 
     });
@@ -65,5 +69,14 @@
         removalDelay: 160,
         preloader: false,
         fixedContentPos: false
+    });
+    // tabs
+    $(function () {
+        $("#tabs").tabs();
+    });
+    // timer
+    
+    $('#timer').countdown('2023/12/31', function (event) {
+        $(this).html(event.strftime('<div> %d<span>:</span> <br> <p>Days</p> </div>' +'<div> %H<span>:</span> <br> <p>Hours</p> </div>' + '<div> %M<span>:</span><br> <p>Min</p></div>' + '<div>%S <br> <p>Sec</p></div>'));
     });
 })(jQuery);
